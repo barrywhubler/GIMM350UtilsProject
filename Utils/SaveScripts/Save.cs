6df352419a51093598267aeaff8908f0b343970b
barrywhubler <45104972+barrywhubler@users.noreply.github.com>
2019-12-06 19:10:17 -0700

    Add files via upload

---
+++ Utils/SaveScripts/Save.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Save {
+
+    public List<int> livingTargetPositions = new List<int>();
+    public List<int> livingTargetsType = new List<int>();
+    public List<float> gameobjectX = new List<float>();
+    public List<float> gameobjectY = new List<float>();
+    public List<float> gameobjectZ = new List<float>();
+
+    public int hits = 0;
+    public int shots = 0;
+
+
+}
+
+
