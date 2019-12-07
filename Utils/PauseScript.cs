949f264aab1dfafe8f877ccf8f330d141a626ec7
barrywhubler <45104972+barrywhubler@users.noreply.github.com>
2019-12-06 19:32:47 -0700

    Add files via upload

---
+++ Utils/PauseScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+    public bool paused = false;
+    public GameObject PauseMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+
+        }
+        else
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+	}
+}
